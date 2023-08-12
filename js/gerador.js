f15af0a82e40517b15ff5041e23167901189f15f
Rodrigo Macedo <workrodmac@gmail.com>
2023-08-12 18:27:35 -0300

    passwordgenerator

---
+++ js/gerador.js
@@ -0,0 +1,31 @@
+let sliderElement = document.querySelector("#slider")
+let buttonElement = document.querySelector("#button")
+
+let sizePassword = document.querySelector("#valor")
+let password = document.querySelector("#password")
+
+let containerPassword = document.querySelector("#container-password")
+
+let charset = "abcdefghijklmnopkrstuvwxyzABCDEFGHIJKLMNOPKRSTUVWYZ0123456789"
+let novaSenha = ""
+
+sizePassword.innerHTML = sliderElement.value
+
+slider.oninput = function () {
+    sizePassword.innerHTML = this.value
+} 
+function geradorSenha() {
+    let pass = ""
+    for(let i = 0, n = charset.length; i < sliderElement.value; ++i){
+      pass += charset.charAt(Math.floor(Math.random() * n))
+      
+    }
+    containerPassword.classList.remove("hide") //tiramos a class hide
+    password.innerHTML = pass
+    novaSenha = pass
+    
+}
+function copiarSenha() {
+    alert("Senha copiada com sucesso!")
+       navigator.clipboard.writeText(novaSenha)
+   }
